5055f2b55b479a3b6882c6bb17c8ced0e2dbf50f
Shubhamjan <shubhamjangale626@gmail.com>
2026-08-02 17:29:12 +0530

    going for pushing image to docker hub

--- src/main/java/com/demo/controller/UserController.java
+++ src/main/java/com/demo/controller/UserController.java
@@ -3,11 +3,13 @@ package com.demo.controller;
 
 import com.demo.entity.User;
 import com.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -20,6 +22,7 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<User> addUser(@RequestBody User user) {
+        log.info("The request received for adding the user");
         return ResponseEntity.ok(userService.addUser(user));
     }
 
