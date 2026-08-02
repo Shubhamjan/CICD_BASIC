465c462b4211f09f7b1ab6a9b06e0de9cd6c3396
Shubhamjan <shubhamjangale626@gmail.com>
2026-08-02 17:03:24 +0530

    log added

--- src/main/java/com/demo/CiCdApplication.java
+++ src/main/java/com/demo/CiCdApplication.java
@@ -1,13 +1,16 @@
 package com.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class CiCdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CiCdApplication.class, args);
+		log.info("CiCd Practices");
 	}
 
 }
