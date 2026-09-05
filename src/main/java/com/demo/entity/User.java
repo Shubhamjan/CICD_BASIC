6bc18fcd5e7e4e6e533d609dbc78121f36d6fbcd
Shubhamjan <shubhamjangale626@gmail.com>
2026-09-05 11:43:14 +0530

    temprarily test for ci

--- src/main/java/com/demo/entity/User.java
+++ src/main/java/com/demo/entity/User.java
@@ -1,13 +1,13 @@
 package com.demo.entity;
 
-import jakarta.persistence.*;
+//import jakarta.persistence.*;
 
-@Entity
-@Table(name= "user")
+//@Entity
+//@Table(name= "user")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
--- src/main/java/com/demo/service/UserService.java
+++ src/main/java/com/demo/service/UserService.java
@@ -2,27 +2,30 @@ package com.demo.service;
 
 
 import com.demo.entity.User;
-import com.demo.repository.UserRepository;
+//import com.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+//    @Autowired
+//    private UserRepository userRepository;
 
-    private final List<User> users = new ArrayList<>();
+    private List<User> users = new ArrayList<>();
 
     public User addUser(User user) {
 
-        return userRepository.save(user);
+//        return userRepository.save(user);
+        users.add(new User(user.getId(),user.getName(), user.getEmail()));
+        return user;
     }
 
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        return users;
     }
 }
